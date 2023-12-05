52ea90b319ad525854d0622fcd2189be58b823f0
Rajat Bansal <rajatbansal16999@gmail.com>
2023-12-05 15:41:53 +0530

    last

--- Project_S/backend/src/app.js
+++ Project_S/backend/src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 // require('./db/dbConnect');
 const mongoose = require("mongoose");
 const app = express();
+const path = require("path");
 
 async function connect() {
   try {
@@ -18,24 +19,28 @@ async function connect() {
 
 connect();
 
-const userRouter = require('./routers/userRoutes');
-const managerRouter = require('./routers/managerRoutes');
-const staffRouter = require('./routers/staffRoutes');
-const foodRouter = require('./routers/foodRoutes');
+const userRouter = require("./routers/userRoutes");
+const managerRouter = require("./routers/managerRoutes");
+const staffRouter = require("./routers/staffRoutes");
+const foodRouter = require("./routers/foodRoutes");
 
 app.use(express.json());
+app.use(express.static("./build"));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
 });
 
 app.use(userRouter);
 app.use(managerRouter);
 app.use(staffRouter);
 app.use(foodRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(4000, function () {
-    console.log(`[+] Server is running at port 4000...`);
+  console.log(`[+] Server is running at port 4000...`);
 });
